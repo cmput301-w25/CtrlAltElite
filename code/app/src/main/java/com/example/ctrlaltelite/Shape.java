d0ba6a8431be9f26b97f6be4489c79d9ab484680
Pragya Das <pragya4@ualberta.ca>
2025-01-30 19:19:01 -0700

    Pushed circle

--- code/app/src/main/java/com/example/ctrlaltelite/Shape.java
+++ code/app/src/main/java/com/example/ctrlaltelite/Shape.java
@@ -3,6 +3,8 @@ package com.example.ctrlaltelite;
 public abstract class Shape {
     private int x;
     private int y;
-
-
+    Shape(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
 }
