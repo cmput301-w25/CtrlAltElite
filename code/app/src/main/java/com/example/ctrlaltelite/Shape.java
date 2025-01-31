3b1c933913914f44b469dbad95218dd34a470bee
Ashbani Kaur <ashbani@ualberta.ca>
2025-01-31 09:21:21 -0700

    Change color

--- code/app/src/main/java/com/example/ctrlaltelite/Shape.java
+++ code/app/src/main/java/com/example/ctrlaltelite/Shape.java
@@ -3,6 +3,7 @@ package com.example.ctrlaltelite;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "Purple";
     Shape(int x, int y){
         this.x = x;
         this.y = y;
